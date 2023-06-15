63db04d2d94fc275a6b82a14cef0e77d6a4a40e6
khawla-nasfi <khawla.nasfi@enstab.ucar.tn>
2023-06-15 16:14:22 -0700

    Hello ,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
 * main - entry poit
-* return always 0 (success)
+* return: always 0 (success)
 */
-int main (void)
+int main(void)
 { 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
