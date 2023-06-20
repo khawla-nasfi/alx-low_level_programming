c7f95964bdd7d2e009477505e50455773a0cb881
khawla-nasfi <khawla.nasfi@enstab.ucar.tn>
2023-06-20 16:39:08 -0700

    Print of alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'Print of alphabet in lowercase'
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+	int j;
+	for (i = 1, i <= 10, i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
