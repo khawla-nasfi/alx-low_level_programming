ab1cbac404cbc93c374544434a208684511fbb81
khawla-nasfi <khawla.nasfi@enstab.ucar.tn>
2023-06-20 14:11:34 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
+#include"main.c"
 /**
  * main Entry point
+ * Description: 'prints _putchar to stdout'
  * Return: Always 0
  */
 int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
