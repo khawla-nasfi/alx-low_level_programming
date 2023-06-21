348d0f998950102d0d41c4e54406ad7e23d7ebe4
khawla-nasfi <khawla.nasfi@enstab.ucar.tn>
2023-06-21 10:07:26 -0700

    prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
  * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
 	int i, j, k;
-
 
 	for (i = 0; i < 10; i++)
 	{
@@ -16,14 +16,14 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
+
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + '0'); 
-			}
-			else if (k >= 10)
+				_putchar(k + '0');
+			} else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -34,4 +34,3 @@ void times_table(void)
 		_putchar('\n');
 	}
 }
-
