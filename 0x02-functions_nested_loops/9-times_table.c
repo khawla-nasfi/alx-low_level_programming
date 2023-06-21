5d6b80d9ae8326b6b8578e86aa1720169114cfa6
khawla-nasfi <khawla.nasfi@enstab.ucar.tn>
2023-06-21 09:53:58 -0700

    Create of table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,9 +21,9 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + '0');
-			} 
-			else if (k >= 10)				
+				_putchar(k + '0'); 
+			}
+			else (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
