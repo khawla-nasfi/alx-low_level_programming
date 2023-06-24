9f3eccc19d01dcbf84ede2fea1d44c9efd8f652c
khawla-nasfi <khawla.nasfi@enstab.ucar.tn>
2023-06-24 12:05:50 -0700

    print hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
@@ -8,7 +9,6 @@ void jack_bauer(void)
 	int i, j;
 
 	i = 0;
-
 
 	while (i < 24)
 	{
@@ -23,6 +23,6 @@ void jack_bauer(void)
 			_putchar('\n');
 			j++;
 		}
-		i++;	
+		i++;
 	}
 }
